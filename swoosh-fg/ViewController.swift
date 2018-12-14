5c0389e0bc259e47764a3931cd4c31148d25fcfb
fguardiola <fernando.guardiola.888@gmail.com>
2018-12-14 11:27:13 +0100

    Set Up skeleton project

---
+++ swoosh-fg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh-fg
+//
+//  Created by 67621177 on 14/12/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
